3879e798342237bd4aa5ea13b65bdfcbc581d482
SureshkumarDuraisamy <sureshkumar.duraisamy@gmail.com>
2025-12-24 14:10:44 +0530

    Prod Setup connectionstring and Swagger

--- service-application.Server/Program.cs
+++ service-application.Server/Program.cs
@@ -67,11 +67,11 @@ app.UseStaticFiles();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
