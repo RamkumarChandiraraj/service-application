880bd7c15ce547d77df9324754c9ce0c3b68848b
Subhanu <subhanuvelusamy@gmail.com>
2025-12-30 14:14:07 +0530

    UserServices

--- Common/RequestDto/UserRequestDto.cs
+++ Common/RequestDto/UserRequestDto.cs
@@ -12,6 +12,7 @@ namespace Common.RequestDto
 {
     public class UserRequestDto : BaseDto
     {
+        public long ID { get; set; }
         public string UserName { get; set; }
         public string? Password { get; set; }
         public string Email { get; set; }
