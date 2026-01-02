8b69e8ebcc50fac951902c0c481356b0ba0a894e
sureshkumar <sureshkumar.duraisamy@gmail.com>
2026-01-02 16:15:21 +0530

    adding reference

--- Data/Entities/Registration.cs
+++ Data/Entities/Registration.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,8 +16,12 @@ namespace Data.Entities
             public override long ID { get; set; }
             public string CompanyName { get; set; }
             public long LocationId { get; set; }
+            [ForeignKey(nameof(LocationId))]
+            public virtual Location Location { get; set; }
             public long ServiceId { get; set; }
-            public string Email { get; set; }
+        [ForeignKey(nameof(ServiceId))]
+        public virtual Service Service { get; set; }
+        public string Email { get; set; }
 
             public long PhoneNumber { get; set; }
             public string Description { get; set; }
