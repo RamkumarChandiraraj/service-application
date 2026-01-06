600ebe50dc9ec4dcf4fd0bd11783bf787a0aadda
dhinakaranvelusamy <velusamydhina@gmail.com>
2026-01-06 15:26:23 +0530

    Mail send with the HTML page

--- Common/ResponseDto/LoginResponseDto.cs
+++ Common/ResponseDto/LoginResponseDto.cs
@@ -10,8 +10,6 @@ namespace Common.ResponseDto
     public class LoginResponseDto
     {
             public string Token { get; set; }
-            public long UserId { get; set; }
-            public string UserName { get; set; }
-            public string Role { get; set; }
+            
     }
 }
